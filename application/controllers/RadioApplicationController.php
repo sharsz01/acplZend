81fe088683626cb78cc62ae1f265b430aa5ec169
sharsz01@students.ipfw.edu <theMaster@192.168.1.12>
2015-10-23 21:59:10 -0400

    Added Zend library to library directory in project.

--- application/controllers/RadioApplicationController.php
+++ application/controllers/RadioApplicationController.php
@@ -54,7 +54,6 @@ class RadioApplicationController extends Zend_Controller_Action {
                     'mailTo' => $otherInfoVals['mailTo'],
                 ));
 
-
                 // ==============================================
                 //$user->createUser($request->getPost());
                 return $this->_helper->redirector('radio-form');
@@ -62,41 +61,4 @@ class RadioApplicationController extends Zend_Controller_Action {
         }
         $this->view->form = $form;
     }
-
-    // temp finished - adds new user record for the radio application form
-    // will need to modify when the database table for users is changed to use
-    // the new form fields that were added
-    public function dbPostUser($user, $request) {
-        $listenerVals = $request->getPost()['listener'];
-        $contactVals = $request->getPost()['contact'];
-        $otherInfoVals = $request->getPost()['otherInfo'];
-        $statementVals = $request->getPost()['statement'];
-
-        $user->createUser(array(
-            'name' => $listenerVals['firstName'],
-            'birthday' => $listenerVals['birthdate'],
-            'street' => $listenerVals['address'],
-            'phone' => $listenerVals['primaryPhone'],
-            'city' => $listenerVals['city'],
-            'state' => $listenerVals['state'],
-            'zip' => $contactVals['zip'],
-            'contactName' => $contactVals['firstName'],
-            'contactRelationship' => $contactVals['relationship'],
-            'contactStreet' => $contactVals['address'],
-            'contactPhone' => $contactVals['primaryPhone'],
-            'contactCity' => $contactVals['city'],
-            'contactState' => $contactVals['state'],
-            'contactZip' => $contactVals['zip'],
-            'contactEmail' => $contactVals['email'],
-            'disability' => $otherInfoVals['disability'],
-            'howLearn' => $otherInfoVals['howLearn'],
-            'race' => $otherInfoVals['race'],
-            'income' => $otherInfoVals['income'],
-            'inHomeNum' => $otherInfoVals['numInHome'],
-            'signature' => $statementVals['signature'],
-            'dateSigned' => $statementVals['signatureDate'],
-            'mailTo' => $otherInfoVals['mailTo'],
-        ));
-    }
-
 }
