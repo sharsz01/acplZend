9388cb1b7c07340bb0e9634aec7cbc3598b65f80
sharsz01 email = sharsz01@students.ipfw.edu <theMaster@192.168.1.8>
2015-11-06 08:16:38 -0500

    Updated Confirm page layout and added Edit button to go back to form for editing.

--- application/forms/Confirm.php
+++ application/forms/Confirm.php
@@ -11,6 +11,12 @@ class Application_Form_Confirm extends Zend_Form
         // Set the method for the display form to POST
         $this->setMethod('post');
         
+        $this->addElement('button', 'cancel', array(
+            'ignore' => true,
+            'label' => "Edit",
+            'onclick' => "history.back()"
+        ));
+        
         // Add the submit button
         $this->addElement('submit', 'submit', array(
             'ignore' => true,
