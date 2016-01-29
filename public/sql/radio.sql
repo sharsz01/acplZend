dd8a178ad458410f0a2e2b5815762d84b05fe696
alilley <shadowgandalf77@gmail.com>
2016-01-29 12:04:25 -0500

    updated tables
    added several fields to radio table.
    added date and ip registered to users and organization table
    added notes fields

---
+++ public/sql/radio.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 29, 2016 at 05:58 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `audioreadingservice`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radio`
+--
+
+CREATE TABLE `radio` (
+  `radioId` int(12) NOT NULL,
+  `controlNum` int(24) NOT NULL,
+  `modelNum` varchar(30) COLLATE latin1_general_ci NOT NULL,
+  `manufacturer` varchar(30) COLLATE latin1_general_ci NOT NULL,
+  `dateOfPurchase` date NOT NULL,
+  `status` varchar(10) COLLATE latin1_general_ci NOT NULL,
+  `headphones` varchar(10) COLLATE latin1_general_ci NOT NULL,
+  `battery` varchar(10) COLLATE latin1_general_ci NOT NULL,
+  `wave` varchar(10) COLLATE latin1_general_ci NOT NULL,
+  `radioCondition` varchar(30) COLLATE latin1_general_ci NOT NULL,
+  `notes` varchar(512) COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
