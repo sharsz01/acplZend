1c977842da4dace51081d888362d9dd881696eae
brewdd02 <brewdd02@students.ipfw.edu>
2016-01-28 19:59:56 -0500

    Update export.php
    Separated the html and php so I can use headers to download the file instead.

--- importExport/export.php
+++ importExport/export.php
@@ -3,31 +3,20 @@
 	//if is used to react to the button being pressed
 	if (isset($_POST['button1']))
 	{
-		//specify the filename
-		//date is used to give each file a unique name (h = hour, i = minute, s = second)
+		//set up filename and tell the browser to download said filename instead of placing it in root
 		$file = 'backupUsers_' . date('His') . '.csv';
-		$output = fopen($file, 'w');
+		header('Content-Disposition: attachment; filename='.$file);
+		$output = fopen('php://output', 'w');
 		
 		//open the connection to the database
 		$link = mysql_connect("localhost", "root", "") or die("Cannot Connect");
 		mysql_select_db("audioreadingservice", $link) or die("Cannot select database");
 		
 		//use a query to get all rows from the table user
+		//to add support for other tables change user to a differnt table name
 		$rows = mysql_query('SELECT * FROM user');
 		
 		//loop over the query and put the results into the .csv file
 		while ($row = mysql_fetch_assoc($rows)) fputcsv($output, $row);
-		
 	}
 ?>
-
-<html>
-	<head>
-		<title>Export</title>
-	</head>
-	<body>
-		<form method="POST" action=''>
-			<input type="submit" name="button1" value="My Button">
-		</form>
-	</body>
-</html>
