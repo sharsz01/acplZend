41a820946720de132995fb618a264dc700ea57ba
sharsz01 email = sharsz01@students.ipfw.edu <theMaster@masterSZS.wireless.ipfw.edu>
2016-02-12 11:35:05 -0500

    Added new individual, update individual, new organization and update organization ajax pages

---
+++ ajax/updateIndividual.php
@@ -0,0 +1,32 @@
+<?php
+
+
+$db = mysqli_connect('localhost', 'root', '', 'audioreadingservice') or die('Error: '.mysqli_connect_error($db));
+
+$return = array();
+
+
+$SQL = "UPDATE user SET firstName='".$_REQUEST['firstName']."', lastName='".$_REQUEST['lastName']
+        . "', birthday='".$_REQUEST['birthday']."', street='".$_REQUEST['street']
+        . "', streetLine2='".$_REQUEST['streetLine2']."', city='".$_REQUEST['city']
+	. "', state='".$_REQUEST['state']."', zip='".$_REQUEST['zip']
+        . "', phone='".$_REQUEST['phone']."', altPhone='".$_REQUEST['altPhone']
+        . "', email='" . $_REQUEST['email'] . "', contactName='" . $_REQUEST['contactName']
+        . "', contactRelationship='" . $_REQUEST['contactRelationship']
+        . "', contactStreet='" . $_REQUEST['contactStreet'] . "', contactStreetLine2='" . $_REQUEST['contactStreetLine2']
+        . "', contactCity='" . $_REQUEST['contactCity'] . "', contactState='" . $_REQUEST['contactState']
+        . "', contactZip='" . $_REQUEST['contactZip'] . "', contactPhone='" . $_REQUEST['contactPhone']
+        . "', contactAltPhone='" . $_REQUEST['contactAltPhone'] . "', contactEmail='" . $_REQUEST['contactEmail']
+        . "', disability='" . $_REQUEST['disability'] . "', howLearn='" . $_REQUEST['howLearn']
+        . "', race='" . $_REQUEST['race'] . "', income='" . $_REQUEST['income']
+        . "', inHomeNum='" . $_REQUEST['inHomeNum'] . "', signature='" . $_REQUEST['signature']
+        . "', dateSigned='" . $_REQUEST['dateSigned'] . "', mailTo='" . $_REQUEST['mailTo']
+	. "' WHERE userId='".$_REQUEST['userId']."';";
+
+$result = mysqli_query($db, $SQL);
+
+$return['success'] = $result;
+
+echo json_encode($return);
+
+?>
---
+++ ajax/updateOrganization.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$db = mysqli_connect('localhost', 'root', '', 'audioreadingservice') or die('Error: '.mysqli_connect_error($db));
+
+$return = array();
+
+
+$SQL = "UPDATE organization SET dateRegistered='" . isset($_REQUEST['dateRegistered']) . "', ipRegistered='" . isset($_REQUEST['ipRegistered'])
+        . "', organizationName='" . isset($_REQUEST['organizationName']) . "', organizationType='" . isset($_REQUEST['organizationType'])
+        . "', firstName='" . isset($_REQUEST['firstName']) . "', lastName='" . isset($_REQUEST['lastName'])
+        . "', positionTitle='" . isset($_REQUEST['positionTitle']) . "', streetAddress='" . isset($_REQUEST['streetAddress'])
+        . "', altAddress='" . isset($_REQUEST['altAddress']) . "', city='" . isset($_REQUEST['city'])
+        . "', state='" . isset($_REQUEST['state']) . "', zip='" . isset($_REQUEST['zip'])
+        . "', phone='" . isset($_REQUEST['phone']) . "', altPhone='" . isset($_REQUEST['altPhone'])
+        . "', email='" . isset($_REQUEST['email']) . "', numRadios='" . isset($_REQUEST['numRadios'])
+        . "', howLearn='" . isset($_REQUEST['howLearn']) . "', signature='" . isset($_REQUEST['signature'])
+        . "', dateSigned='" . isset($_REQUEST['dateSigned']) . "', notes='" . isset($_REQUEST['notes'])
+        . "';";
+
+$result = mysqli_query($db, $SQL);
+
+$return['success'] = $result;
+
+echo json_encode($return);
+
+?>
