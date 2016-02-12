5b35dfa3137043bce211f2798e5058c7561fda9b
sharsz01 email = sharsz01@students.ipfw.edu <theMaster@192.168.1.11>
2016-02-11 21:22:08 -0500

    Comments

--- application/models/User.php
+++ application/models/User.php
@@ -5,6 +5,7 @@ class Application_Model_User
     private $_dbTable;
     
     public function __construct($table) {
+        // Determine if to use the 'user' table or 'organization' table
         if(strcmp($table, "user") == 0){
             $this->_dbTable = new Application_Model_DbTable_User();
         }
@@ -14,10 +15,12 @@ class Application_Model_User
         
     }
     
+    // Insert record to table
     public function createUser($array){
         $this->_dbTable->insert($array);
     }
     
+    // Update record in table
     public function updateUser($array, $id){
         $this->_dbTable->update($array, "userid = $id");
     }
