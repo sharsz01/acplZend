d62fbcb45e29629c2435541cbe2a57a269872926
brewdd02 <brewdd02@students.ipfw.edu>
2016-04-21 15:48:01 -0400

    Create addUser.php
    Add user code for the user management tab. Only admin has access to this.

---
+++ application/admin/userControls/addUser.php
@@ -0,0 +1,87 @@
+<!--allows us to use modal dialog boxes -->
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<?php
+	//variables for the creation of a new user
+	$username = $_POST['newUsername'];
+	$password = $_POST['newUserPassword'];
+	$passwordAgain = $_POST['newUserPasswordAgain'];
+	
+	//set up database connection
+	$link = mysql_connect("localhost", "root", "") or die("Cannot Connect");
+	mysql_select_db("audioreadingservice", $link) or die("Cannot select database");
+	
+	//check if username exists already
+	$res = mysql_query("select * from login_master where username = '".$username."'");
+	if(mysql_num_rows($res) > 0)
+	{
+		//inform the user the username already exists
+		echo '<div id="alreadyExistsModal" class="modal fade" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<h4 class="modal-title">Error</h4>
+						</div>
+						<div class="modal-body">
+							<p>Error: The username already exists.</p>
+						</div>
+						<div class="modal-footer">
+							<button type="button" id="closeBtn" class="btn btn-default" data-dismiss="modal">Close</button>
+						<div>
+					</div>
+				</div>
+			</div>
+			
+			<script>
+				$(\'#alreadyExistsModal\').modal(\'show\');
+				document.getElementById("closeBtn").onclick = function () 
+				{
+					location.href = "../admin.php";
+				};
+			</script>';
+	}
+	else
+	{
+		//check if the passwords match
+		if($password == $passwordAgain)
+		{
+			//add the new user to the database
+			mysql_query("insert into login_master (username,password) values('".$username."','".md5($password)."')");
+		}
+		else
+		{
+			//inform the user the passwords do not match
+			echo '<div id="alreadyExistsModal" class="modal fade" role="dialog">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">Error</h4>
+							</div>
+							<div class="modal-body">
+								<p>Error: The passwords do not match.</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" id="closeBtn" class="btn btn-default" data-dismiss="modal">Close</button>
+							<div>
+						</div>
+					</div>
+				</div>
+				
+				<script>
+					$(\'#alreadyExistsModal\').modal(\'show\');
+					document.getElementById("closeBtn").onclick = function () 
+					{
+						location.href = "../admin.php";
+					};
+				</script>';
+		}
+	}
+	header('Location: '.$_SERVER['HTTP_REFERER']);
+?>
