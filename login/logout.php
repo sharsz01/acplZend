5e9be27bad27c0289db93f9ff9ceda4b1dd2e710
sharsz01 email = sharsz01@students.ipfw.edu <theMaster@192.168.1.9>
2016-04-22 07:29:44 -0400

    Removed old files

--- login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	setcookie("user", "", time() -3600);
-	setcookie("pass", "", time() -3600);
-	setcookie(session_name(), "", time() - 3600);
-	
-	echo "<script>alert('Successfully logged out');window.location.href='loginForm.php';</script>";
-?>
