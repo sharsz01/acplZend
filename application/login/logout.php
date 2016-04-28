4e6f7725123407c7794c155896bee647b692eab9
sharsz01 email = sharsz01@students.ipfw.edu <theMaster@192.168.1.7>
2016-04-28 13:17:34 -0400

    Updated logout to destroy session

--- application/login/logout.php
+++ application/login/logout.php
@@ -1,7 +1,9 @@
 <?php
+        session_start();
 	setcookie("user", "", time() -3600);
 	setcookie("pass", "", time() -3600);
 	setcookie(session_name(), "", time() - 3600);
-	
+	session_destroy();
+        
 	echo "<script>alert('Successfully logged out');window.location.href='loginForm.php';</script>";
 ?>
